d8977b38839942bf20d72e4457ac828a5d9ce573
Jesiel Rocha Lofhagen <jesiel.r.lofhagen@gmail.com>
2020-05-05 14:07:06 -0300

    1.0

---
+++ App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Comments />
+</template>
+
+<script>
+import Comments from './project'
+
+export default {
+  name: 'app',
+  components: {
+    Comments
+  }
+}
+</script>
+
+<style>
+
+</style>
