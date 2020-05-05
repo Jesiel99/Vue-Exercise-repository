e91889b4cd0fe3e07775e5a11e526884c6dbfd6b
Jesiel Rocha Lofhagen <jesiel.r.lofhagen@gmail.com>
2020-05-05 15:39:50 -0300

    1.1

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+ <Projects/>
+</template>
+
+<script>
+import Projects from './components/Projects.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Projects
+  }
+}
+</script>
+
+<style>
+
+</style>
